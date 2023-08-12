96891e53cd431ce9863c8de329fb3f60887e390b
Ay-coder <ayomidebabarinde07@gmail.com>
2023-08-12 13:26:35 +0100

    update1

--- controller/shop.js
+++ controller/shop.js
@@ -44,7 +44,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:8000/seller/activation/${activationToken}`;
+    const activationUrl = `https://ecommerce-hr3c.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
